52787ccd9dc2da6273fc22469047ff0f757223db
amscw <moskwin.a.va@gmail.com>
2018-04-05 22:17:57 +0300

    добавил доки

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 /*
  * File:   main.c
  * Author: moskw
- *
+ * Peripherial Libraries Help: User Guide, p.472 
+ * Configuration registers Help: Datasheet, p.251 
  * Created on 5 апреля 2018 г., 15:30
  */
 
-
 #include <xc.h>
+#include "config.h"
 
 void main(void) {
     return;
